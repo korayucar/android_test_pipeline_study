b421f4e1a49771c79d2de65168a0a1aacca52033
tahakirca <tahakirca@gmail.com>
2017-10-29 23:18:37 +0300

    running test fail

--- app/src/androidTest/java/com/mobilhanem/tahaapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mobilhanem/tahaapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.mobilhanem.tahaapp", appContext.getPackageName());
+        assertEquals("com.mobilhanem.tahaapp2", appContext.getPackageName());
     }
 }
